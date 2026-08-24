6325c63a73aaaf0a894f9ebe3e26cd3343675f89
msvanson <320430345+msvanson@users.noreply.github.com>
2026-08-24 01:06:49 -0500

    initial cfb team fantasy website

---
+++ app/nav.js
@@ -0,0 +1,3 @@
+import Link from 'next/link';
+export const nav=[['/','Standings'],['/live','Live'],['/rosters','Rosters'],['/teams','Teams / Waivers'],['/weekly','Weekly'],['/league','League']];
+export function Nav(){return <nav className="nav">{nav.map(([h,l])=><Link key={h} href={h}>{l}</Link>)}</nav>}
